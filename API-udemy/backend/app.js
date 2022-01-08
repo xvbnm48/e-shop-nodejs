18f0ba2330fc4a2ccb512ca7ab0e684339a01c31
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-08 23:02:31 +0700

    INSTALL CORES,refactor struktur folder, membuat categori model dan router dan products

--- API-udemy/backend/app.js
+++ API-udemy/backend/app.js
@@ -3,6 +3,11 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const cors = require('cors');
+
+app.use(cors());
+app.options('*', cors());
+
 
 require('dotenv/config')
 
@@ -13,50 +18,18 @@ const api = process.env.API_URL;
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {
-        type: Number,
-        required: true,
-    }
-})
-
-const Product = mongoose.model('Product', productSchema);
-
-
-app.get(`${api}/products`, async  (req, res) => {
-    const productList = await Product.find();
-    if(!productList) {
-        res.status(500).json({success: false, message: 'No products found'});
-    }
-    res.send(productList);
-});
-
-app.post(`${api}/products`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    });
-
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct);
-    }).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        });
-    });
-
-});
+//routers
+const productsRouter = require('./routers/products');
+const categoriesRouter = require('./routers/categories');
+app.use(`${api}/products`, productsRouter);
+app.use(`${api}/categories`, categoriesRouter);
 
 mongoose.connect(process.env.CONNECTION_STRING, {
     // userNewUrlParser: true,
     dbName: 'eshop-database'
 })
 .then(() => {
-    console.log('Connected to database');
+    console.log('Success Connected to database');
 })
 .catch(err => {
     console.log('Error: ', err);
---
+++ API-udemy/backend/routers/categories.js
@@ -0,0 +1,43 @@
+const {Category} = require('../models/category');
+const express = require('express');
+const router = express.Router();
+
+router.get(`/`, async (req, res) => {
+    const categoryList = await Category.find();
+    if(!categoryList) {
+        res.status(500).json({success: false, message: 'No categories found'});
+    }
+    res.send(categoryList);
+});
+
+router.post(`/`, async (req, res) => {
+    let category = new Category({
+        name: req.body.name,
+        icon: req.body.icon,
+        color: req.body.color,
+        image: req.body.image
+    });
+
+    category = await category.save();
+
+    if(!category){
+        return res.status(500).send('the category cannot be created');
+    }
+
+    res.send(category);
+});
+
+router.delete(`/:id`, async (req, res) => {
+    // const category = await Category.findByIdAndDelete(req.params.id);
+    Category.findByIdAndRemove(req.params.id).then(category => {
+        if(category) {
+            return res.status(200).json({success: true, message: 'Category deleted'});
+        } else {
+            return res.status(404).json({success: false, message: 'Category not found'});
+        }
+    }).catch(err => {
+        return res.status(400).json({success: false, message: err});
+    });
+});
+
+module.exports =  router;
