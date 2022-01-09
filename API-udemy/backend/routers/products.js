cad87474d0e122c0455f48c16ccfaf3cfe8c190a
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-09 23:15:29 +0700

    berhasil membuat product js post dengan async , update readme

--- API-udemy/backend/routers/products.js
+++ API-udemy/backend/routers/products.js
@@ -1,32 +1,39 @@
-const {Product} = require('../models/products');
-const express = require('express');
+const { Product } = require("../models/products");
+const { Category } = require("../models/category");
+const express = require("express");
 const router = express.Router();
 
-
-router.get(`/`, async  (req, res) => {
-    const productList = await Product.find();
-    if(!productList) {
-        res.status(500).json({success: false, message: 'No products found'});
-    }
-    res.send(productList);
+router.get(`/`, async (req, res) => {
+  const productList = await Product.find();
+  if (!productList) {
+    res.status(500).json({ success: false, message: "No products found" });
+  }
+  res.send(productList);
 });
 
-router.post(`/`, (req, res) => {
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock
-    });
+router.post(`/`, async (req, res) => {
+  const category = await Category.findById(req.body.category);
+  if (!category) return res.status(400).send("Invalid Category");
 
-    product.save().then((createdProduct) => {
-        res.status(201).json(createdProduct);
-    }).catch((err) => {
-        res.status(500).json({
-            error: err,
-            success: false
-        });
-    });
+  // ambil data dari body request
+  let product = new Product({
+    name: req.body.name,
+    description: req.body.description,
+    richDescription: req.body.richDescription,
+    image: req.body.image,
+    brand: req.body.brand,
+    price: req.body.price,
+    category: req.body.category,
+    countInStock: req.body.countInStock,
+    rating: req.body.rating,
+    numReviews: req.body.numReviews,
+    isFeatured: req.body.isFeatured,
+  });
 
+  // simpan ke database
+  product = await product.save();
+  if (!product) return res.status(500).send("the product cannot be created");
+  res.status(200).send(product);
 });
 
-module.exports =  router;
+module.exports = router;
--- readme.md
+++ readme.md
@@ -2,3 +2,18 @@ this is node js course
 link [NODE JS E SHOP](https://www.udemy.com/course/nodejs-build-complete-ecommerce-web-api/learn/lecture/24250940#overview)
 
 now at part 24
+ini kalo mau pakai post tanpa async
+
+```javascript
+ product
+    .save()
+    .then((createdProduct) => {
+      res.status(201).json(createdProduct);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        error: err,
+        success: false,
+      });
+    });
+```
