ea49deec44e408ef6e746fb0bac557e726f35ec0
xvbnm48 <muhamadfarizwisnuprananda@gmail.com>
2022-01-19 23:13:20 +0700

    get user by id

--- API-udemy/backend/routers/users.js
+++ API-udemy/backend/routers/users.js
@@ -1,9 +1,10 @@
 const { User } = require("../models/user");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 router.get("/", async (req, res) => {
-  const userList = await User.find();
+  const userList = await User.find().select("-passwordHash");
   if (!userList) {
     res.status(500).json({ success: false });
   }
@@ -14,7 +15,7 @@ router.post(`/`, async (req, res) => {
   let user = new User({
     name: req.body.name,
     email: req.body.email,
-    passwordHash: req.body.passwordHash,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
     phone: req.body.phone,
     isAdmin: req.body.isAdmin,
     street: req.body.street,
@@ -33,4 +34,11 @@ router.post(`/`, async (req, res) => {
   res.send(user);
 });
 
+router.get("/:id", async (req, res) => {
+  const user = await User.findById(req.params.id).select("-passwordHash");
+  if (!user) {
+    return res.status(404).send("the user with the given ID was not found");
+  }
+  res.status(200).send(user);
+});
 module.exports = router;
